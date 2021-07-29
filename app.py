b20c3479f62091eca2bb7638f4c7374a54ddf850
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-07-29 08:51:16 +0200

    add read file function

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def read_file(file):
+    img_bytes = file.read()
+    class_name, class_id = get_prediction(image_bytes=img_bytes)
+    len_glob = len(glob.glob('static/uploads/*.*'))
+    filename = f"{len_glob}_{class_name}_{file.filename.split('/')[-1]}"
+    file.seek(0)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return class_name, class_id, filename
