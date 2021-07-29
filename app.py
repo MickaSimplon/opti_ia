27bc72d7c786839867bbc82fc56a55bf597848a6
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-07-29 08:50:07 +0200

    import glob & shutil

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
-
 from flask import Flask, render_template, request, redirect
-
 from inference import get_prediction
-
+import shutil
+import glob
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads/'
